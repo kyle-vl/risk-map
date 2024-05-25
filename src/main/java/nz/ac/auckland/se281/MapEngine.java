1ff3505e18af054fa3f5ef8e75da44db0991b5ab
Kyle <klim385@aucklanduni.ac.nz>
2024-05-25 21:08:24 +1200

    showInfoCountry method handles invalid inputs

--- src/main/java/nz/ac/auckland/se281/MapEngine.java
+++ src/main/java/nz/ac/auckland/se281/MapEngine.java
@@ -39,20 +39,25 @@ public class MapEngine {
 
   /** this method is invoked when the user run the command info-country. */
   public void showInfoCountry() {
-    // Ask user for input
-    MessageCli.INSERT_COUNTRY.printMessage();
-    String input = Utils.readStringInput();
+    // Repeat until user enters valid country name
+    while (true) {
+      // Ask user for input
+      MessageCli.INSERT_COUNTRY.printMessage();
+      String input = Utils.capitalizeFirstLetterOfEachWord(Utils.readStringInput());
 
-    // Find matching country name
-    for (Country country : graph.getAdjacencyMap().keySet()) {
-      if (country.getCountryName().equals(input)) {
-        // Get and print country details
-        String countryName = country.getCountryName();
-        String continent = country.getContinent();
-        String borderTax = String.valueOf(country.getBorderTax());
-        MessageCli.COUNTRY_INFO.printMessage(countryName, continent, borderTax);
-        return;
+      // Find matching country name
+      for (Country country : graph.getAdjacencyMap().keySet()) {
+        if (country.getCountryName().equals(input)) {
+          // Get and print country details
+          String countryName = country.getCountryName();
+          String continent = country.getContinent();
+          String borderTax = String.valueOf(country.getBorderTax());
+          MessageCli.COUNTRY_INFO.printMessage(countryName, continent, borderTax);
+          return;
+        }
       }
+      // If country not found, print error message and loop again
+      MessageCli.INVALID_COUNTRY.printMessage(input);
     }
   }
 
