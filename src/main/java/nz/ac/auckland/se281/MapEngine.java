20d9b7bf4120d255bf11ff9cc307edd386cbdcf2
Kyle <klim385@aucklanduni.ac.nz>
2024-05-25 20:53:44 +1200

    loadMap method uses addEdge to put countries and adjacencies graph

--- src/main/java/nz/ac/auckland/se281/MapEngine.java
+++ src/main/java/nz/ac/auckland/se281/MapEngine.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 /** This class is the main entry point. */
 public class MapEngine {
+  private Graph graph;
 
   public MapEngine() {
+    this.graph = new Graph();
     loadMap();
   }
 
@@ -13,6 +15,26 @@ public class MapEngine {
   private void loadMap() {
     List<String> countries = Utils.readCountries();
     List<String> adjacencies = Utils.readAdjacencies();
+    for (int i = 0; i < countries.size(); i++) {
+      // Read line and split country into parts
+      String countryString = countries.get(i);
+      String[] countryParts = countryString.split(",");
+
+      // Use parts to create new Country object
+      String countryName = countryParts[0];
+      String continent = countryParts[1];
+      int borderTax = Integer.parseInt(countryParts[2]);
+      Country newCountry = new Country(countryName, continent, borderTax);
+
+      // Read line and split adjacencies into parts
+      String adjacentString = adjacencies.get(i);
+      String[] adjacentParts = adjacentString.split(",");
+
+      // Add the new Country with all it's adjacents
+      for (int j = 0; j < adjacentParts.length; j++) {
+        graph.addEdge(newCountry, adjacentParts[j]);
+      }
+    }
   }
 
   /** this method is invoked when the user run the command info-country. */
