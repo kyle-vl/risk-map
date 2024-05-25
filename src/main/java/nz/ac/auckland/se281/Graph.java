65394f663e9ab14c805abab76a46794390e4cf80
Kyle <klim385@aucklanduni.ac.nz>
2024-05-26 00:51:22 +1200

    BreadthFirstTraversal method added to Graph class

--- src/main/java/nz/ac/auckland/se281/Graph.java
+++ src/main/java/nz/ac/auckland/se281/Graph.java
@@ -1,9 +1,11 @@
 package nz.ac.auckland.se281;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 
 class Graph {
   private Map<Country, List<String>> adjacencyMap;
@@ -32,5 +34,31 @@ class Graph {
       }
     }
     return null;
+  }
+
+  public List<Country> breadthFirstTraversal(Country source, Country destination) {
+    List<Country> visited = new ArrayList<>();
+    Queue<Country> queue = new LinkedList<>();
+    queue.add(source);
+    visited.add(source);
+
+    while (!queue.isEmpty()) {
+      Country currentCountry = queue.poll();
+      if (currentCountry.equals(destination)) {
+        // If destination reached, stop and return the visited list
+        return visited;
+      }
+
+      List<String> adjacents = getAdjacencyMap().get(currentCountry);
+      for (String adjacentName : adjacents) {
+        Country adjacent = getCountryByName(adjacentName);
+        if (adjacent != null && !visited.contains(adjacent)) {
+          visited.add(adjacent);
+          queue.add(adjacent);
+        }
+      }
+    }
+
+    return visited;
   }
 }
